2a5a67cdfdabdb222dfa7fa6d9ebf9b64cbfd0cc
jekyll0_0 <ildar2571@yandex.ru>
2017-01-26 15:21:34 +0400

    Date:		26.01.2017 Author:		Kasimov Ildar Description:	The folder docs/ was deleted from the repository.

--- CAIPLib/docs/html/search/classes_0.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['tcolor',['TColor',['../struct_t_color.html',1,'']]],
-  ['timage',['TImage',['../struct_t_image.html',1,'']]],
-  ['ttgaheader',['TTGAHeader',['../struct_t_t_g_a_header.html',1,'']]]
-];
--- CAIPLib/docs/html/search/functions_2.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['max',['Max',['../_c_a_i_p_utils_8h.html#a0911cabbf4e2a4fc9fd2ddcea8035788',1,'CAIPUtils.h']]],
-  ['min',['Min',['../_c_a_i_p_utils_8h.html#a08635059ad5432d0d4719d57e82395c3',1,'CAIPUtils.h']]]
-];
